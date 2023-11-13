b4fbaf897f001a22c5b4b162b2b35bc4dafed92d
ricardorosa <rjrosa@outlook.com>
2023-11-13 10:53:18 +0000

    Committing tests on author adn book repository delete using spring data JPA.

--- src/test/java/com/rr/databese1/repositories/AuthorRepositoryIntegrationTests.java
+++ src/test/java/com/rr/databese1/repositories/AuthorRepositoryIntegrationTests.java
@@ -10,6 +10,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.Optional;
 
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
